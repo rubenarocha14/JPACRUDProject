71e564b17abae3a98c7ed274e8d99145a0f6fe46
rubenarocha14 <rubenarocha14@gmail.com>
2022-10-14 16:10:27 -0600

    db connected and junit test green

---
+++ JPAGolfSocial/src/main/java/com/skilldistillery/golfsocial/entities/Player.java
@@ -0,0 +1,50 @@
+package com.skilldistillery.golfsocial.entities;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Player {
+	
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column (name = "first_name")
+	private String firstName;
+	
+	public Player() {
+		
+	}
+
+	public Player(int id, String firstName) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	@Override
+	public String toString() {
+		return "Player [id=" + id + ", firstName=" + firstName + "]";
+	}
+}
---
+++ JPAGolfSocial/src/test/java/com/skilldistillery/golfsocial/entities/JPASocialTest.java
@@ -0,0 +1,50 @@
+package com.skilldistillery.golfsocial.entities;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class JPASocialTest {
+	
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private Player player1;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("JPAGolfSocial");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		player1 = em.find(Player.class, 3);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		em.close();
+		player1= null;
+	}
+
+	@Test
+	void test_Player_mapping() {
+		assertNotNull(player1);
+		assertEquals("Matt", player1.getFirstName());
+	}
+
+}
