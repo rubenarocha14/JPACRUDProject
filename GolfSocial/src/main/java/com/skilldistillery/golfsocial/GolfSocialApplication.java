815c28a9c7f10a0f308397aa681bfaa66d5632d0
rubenarocha14 <rubenarocha14@gmail.com>
2022-10-14 16:29:03 -0600

    created spring boot pjt

---
+++ GolfSocial/src/main/java/com/skilldistillery/golfsocial/GolfSocialApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.golfsocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GolfSocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GolfSocialApplication.class, args);
+	}
+
+}
---
+++ GolfSocial/src/test/java/com/skilldistillery/golfsocial/GolfSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.golfsocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GolfSocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
