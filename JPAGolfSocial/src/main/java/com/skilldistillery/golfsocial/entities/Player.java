d0553b8426ab893db49cb7c4a40ea5a4303522df
rubenarocha14 <rubenarocha14@gmail.com>
2022-10-15 08:20:11 -0600

    db table complete, Player entity complete

--- JPAGolfSocial/src/main/java/com/skilldistillery/golfsocial/entities/Player.java
+++ JPAGolfSocial/src/main/java/com/skilldistillery/golfsocial/entities/Player.java
@@ -17,6 +17,17 @@ public class Player {
 	@Column (name = "first_name")
 	private String firstName;
 	
+	@Column (name = "last_name")
+	private String lastName;
+	
+	private  int handicap;
+	
+	private String city;
+	
+	private String state;
+	
+	private String image;
+	
 	public Player() {
 		
 	}
